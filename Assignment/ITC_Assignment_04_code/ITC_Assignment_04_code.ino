aa106916b8546b2d018be62360eec8c9ae4add44
sn-nast <sunsun.natchanon@gmail.com>
2021-10-02 03:09:26 +0700

    Assignment 3 - Auto move bar

--- Assignment/ITC_Assignment_04_code/ITC_Assignment_04_code.ino
+++ Assignment/ITC_Assignment_04_code/ITC_Assignment_04_code.ino
@@ -1,7 +1,38 @@
-void setup() {
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define OLED_RESET -1
+
+Adafruit_SSD1306 OLED(OLED_RESET);
+
+void setup(){
+    Serial.begin(115200);
+    OLED.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
 }
 
-void loop() {
+int i = 0;
+int Ro = 1;
+void loop()
+{
+
+struct Bar{
+    int lenght = 25;
+    int posX;
+    int posY;
+} myBar;
+
+myBar.posX = 0;
+myBar.posY = 31;
+
+    OLED.clearDisplay();
+    OLED.drawLine(i, myBar.posY, i + myBar.lenght, myBar.posY, WHITE);
+    OLED.display();
+    Serial.println(i);
+    if(Ro == 1) {i++;}
+    else if (Ro == 2) {i--;}
+
+    if(Ro == 1 && i == 127 - myBar.lenght) {Ro = 2;}
+    else if (Ro == 2 && i == 0) {Ro = 1;}
 
 }
