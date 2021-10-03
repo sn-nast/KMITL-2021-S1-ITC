bc09a0c1c45c0be17728acff33bdb03f629cd2da
sn-nast <sunsun.natchanon@gmail.com>
2021-10-03 20:30:34 +0700

    Move and Ball completed

--- Assignment/ITC_Assignment_04_code/ITC_Assignment_04_code.ino
+++ Assignment/ITC_Assignment_04_code/ITC_Assignment_04_code.ino
@@ -14,18 +14,18 @@ Adafruit_SSD1306 OLED(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 int readResistor_now;
 int readResistor_last;
 
-int ball_posX, ball_posY;
-const int ball_radius = 2;
-int ball_moveX = 1, ball_moveY = 1;  
-const int ball_addX = 1,  ball_addY = 1 ;
-
 struct Bar{
-    unsigned int lenght;
-    unsigned int posX;
-    unsigned int posY;
-    unsigned int turn;
-    unsigned int last_posX;
+    int lenght;
+    int posX, posY, turn;
 } myBar;
+
+struct Ball{
+    int posX, posY;
+    int radius;
+    int moveX, moveY;  
+    int addX, addY;
+    int timeNewBall = 100; 
+} ball;
 
 void drawBall();
 
@@ -33,24 +33,26 @@ void setup(){
     randomSeed(analogRead(A0));
     Serial.begin(115200);
     OLED.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    // OLED.display();
 
     //Set up myBar
     myBar.lenght = 25;
     myBar.posY = 28;
-    myBar.last_posX = 0;
+    myBar.turn = 1 ;
 
     //Set up controller
     pinMode(A2, INPUT);
     readResistor_last = 0;
-    myBar.last_posX = myBar.posX;
 
     // Set up ball
-    ball_posX = random(0 + ball_radius, SCREEN_WIDTH - ball_radius);
-    ball_posY = random(0, 5);
+    ball.posX = random(0 + ball.radius, SCREEN_WIDTH - ball.radius);
+    ball.posY = random(0, ball.radius );
+    ball.radius = 2;
+    ball.addX = 1;
+    ball.addY = 1;
+    ball.moveX = ball.addX;
+    ball.moveX = ball.addY;
 
     OLED.clearDisplay();
-
 }
 
 void loop() {
@@ -60,49 +62,59 @@ void loop() {
     OLED.clearDisplay();
     
     // Moving by Resistor 
-    Serial.println(abs(readResistor_now-readResistor_last));
     if( abs(readResistor_now - readResistor_last) > voltageSet ){
         readResistor_last = readResistor_now;
-        myBar.last_posX = myBar.posX;
         myBar.posX = readResistor_now / voltageSet;
     }
     OLED.drawLine(myBar.posX, myBar.posY, (myBar.posX + myBar.lenght), myBar.posY, WHITE);
 
     // Ball move
     drawBall();
-    ball_posX += ball_moveX;
-    ball_posY += ball_moveY;
-    if(ball_posX - ball_radius <= 0){
-        ball_posX = ball_radius;
-        ball_moveX = ball_addX ;
+    if(myBar.turn == 1) {
+        delay(1);
+        ball.timeNewBall--;
+        if (ball.timeNewBall == 0) {
+            myBar.turn = 0;
+            ball.timeNewBall = 100;
+        }
     }
-    if(ball_posX + ball_radius >= SCREEN_WIDTH){
-        ball_posX = SCREEN_WIDTH - ball_radius;
-        ball_moveX = -ball_addX;
+    else {
+        ball.posX += ball.moveX;
+        ball.posY += ball.moveY;
     }
-    if(ball_posY - ball_radius <= 0){
-        ball_moveY = ball_addY ;
+
+    // Direction
+    if(ball.posX - ball.radius <= 0){
+        ball.posX = ball.radius;
+        ball.moveX = ball.addX ;
     }
-    if(ball_posY + ball_radius >= myBar.posY){
-        // if(ball_posX >= myBar.posX && ball_posX <= myBar.posX + myBar.lenght && 
-        // ball_posY + ball_radius == myBar.posY){
-            ball_moveY = -ball_addY ;
-        // }
-        // else if (ball_posY - ball_radius >= SCREEN_HEIGHT) {
-        //     OLED.clearDisplay();
-        //     OLED.setCursor(5,6);
-        //     OLED.setTextSize(2);
-        //     OLED.setTextColor(WHITE);
-        //     OLED.println("Game over!");
-        //     OLED.display();
-        //     delay(5000);
-        //     setup();
-        // }
+    if(ball.posX + ball.radius >= SCREEN_WIDTH){
+        ball.posX = SCREEN_WIDTH - ball.radius;
+        ball.moveX = -ball.addX;
+    }
+    if(ball.posY - ball.radius <= 0){
+        ball.moveY = ball.addY ;
+    }
+    if(ball.posY + ball.radius >= myBar.posY){
+        if(ball.posX >= myBar.posX && ball.posX <= myBar.posX + myBar.lenght && 
+        ball.posY + ball.radius == myBar.posY){
+            ball.moveY = -ball.addY ;
+        }
+        else if (ball.posY - ball.radius >= SCREEN_HEIGHT) {
+            OLED.clearDisplay();
+            OLED.setCursor(5,6);
+            OLED.setTextSize(2);
+            OLED.setTextColor(WHITE);
+            OLED.println("Game over!");
+            OLED.display();
+            delay(3000);
+            setup();
+        }
     }
     OLED.display();
 }
 
 void drawBall(){
-    OLED.drawCircle(ball_posX, ball_posY, ball_radius, WHITE);
-    OLED.fillCircle(ball_posX, ball_posY, ball_radius, WHITE);
+    OLED.drawCircle(ball.posX, ball.posY, ball.radius, WHITE);
+    OLED.fillCircle(ball.posX, ball.posY, ball.radius, WHITE);
 }
