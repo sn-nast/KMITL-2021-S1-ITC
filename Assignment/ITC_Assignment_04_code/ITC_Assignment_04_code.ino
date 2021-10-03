32f447457ec04b16fc08deb617abff7732b94dfe
sn-nast <sunsun.natchanon@gmail.com>
2021-10-03 19:29:50 +0700

    Major change - set new Parameter in Adafruit

--- Assignment/ITC_Assignment_04_code/ITC_Assignment_04_code.ino
+++ Assignment/ITC_Assignment_04_code/ITC_Assignment_04_code.ino
@@ -1,21 +1,23 @@
+#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <math.h>
-#define OLED_RESET -1
-const short max_weight = 127, max_height = 31;
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+Adafruit_SSD1306 OLED(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 int readResistor_now;
 int readResistor_last;
-unsigned long time_now;
-unsigned long time_last;
 
 int ball_posX, ball_posY;
-const int ball_radius = 3;
-int ball_moveX = 1, ball_moveY = 1; 
-
-
-Adafruit_SSD1306 OLED(OLED_RESET);
+const int ball_radius = 2;
+int ball_moveX = 1, ball_moveY = 1;  
+const int ball_addX = 1,  ball_addY = 1 ;
 
 struct Bar{
     unsigned int lenght;
@@ -25,15 +27,17 @@ struct Bar{
     unsigned int last_posX;
 } myBar;
 
+void drawBall();
+
 void setup(){
     randomSeed(analogRead(A0));
     Serial.begin(115200);
     OLED.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    OLED.display();
+    // OLED.display();
 
     //Set up myBar
     myBar.lenght = 25;
-    myBar.posY = 31;
+    myBar.posY = 28;
     myBar.last_posX = 0;
 
     //Set up controller
@@ -42,51 +46,58 @@ void setup(){
     myBar.last_posX = myBar.posX;
 
     // Set up ball
-    ball_posX = random(0 + ball_radius, max_weight - ball_radius);
-    ball_posY = random(0 + ball_radius, (1/3)*max_height);
+    ball_posX = random(0 + ball_radius, SCREEN_WIDTH - ball_radius);
+    ball_posY = random(0, 5);
+
+    OLED.clearDisplay();
+
 }
 
 void loop() {
     readResistor_now = analogRead(A2);
-    double voltageSet = 1024.0/((max_weight - 1) - myBar.lenght);
+    double voltageSet = 1024.0/((SCREEN_WIDTH - 1) - myBar.lenght);
 
     OLED.clearDisplay();
     
     // Moving by Resistor 
-        Serial.println(voltageSet);
-        if( abs(readResistor_now-readResistor_last) > voltageSet ){
-            readResistor_last = readResistor_now;
-            myBar.last_posX = myBar.posX;
-            myBar.posX = readResistor_now / voltageSet;
-        }
-            OLED.drawLine(myBar.posX, myBar.posY, (myBar.posX + myBar.lenght), myBar.posY, WHITE);
+    Serial.println(abs(readResistor_now-readResistor_last));
+    if( abs(readResistor_now - readResistor_last) > voltageSet ){
+        readResistor_last = readResistor_now;
+        myBar.last_posX = myBar.posX;
+        myBar.posX = readResistor_now / voltageSet;
+    }
+    OLED.drawLine(myBar.posX, myBar.posY, (myBar.posX + myBar.lenght), myBar.posY, WHITE);
+
     // Ball move
     drawBall();
     ball_posX += ball_moveX;
     ball_posY += ball_moveY;
-    if(ball_posX - ball_radius == 0){
-        ball_moveX = 1;
+    if(ball_posX - ball_radius <= 0){
+        ball_posX = ball_radius;
+        ball_moveX = ball_addX ;
     }
-    if(ball_posX + ball_radius == max_weight){
-        ball_moveX = -1;
+    if(ball_posX + ball_radius >= SCREEN_WIDTH){
+        ball_posX = SCREEN_WIDTH - ball_radius;
+        ball_moveX = -ball_addX;
     }
-    if(ball_posY - ball_radius == 0){
-        ball_moveY = 1;
+    if(ball_posY - ball_radius <= 0){
+        ball_moveY = ball_addY ;
     }
-    if(ball_posY + ball_radius == max_height){
-        if(ball_posX > myBar.posX && ball_posX <= myBar.posX + myBar.lenght){
-            ball_moveY = -1;
-        }
-        else {
-            OLED.clearDisplay();
-            OLED.setCursor(5,6);
-            OLED.setTextSize(2);
-            OLED.setTextColor(WHITE);
-            OLED.println("Game over!");
-            OLED.display();
-            delay(5000);
-            setup();
-        }
+    if(ball_posY + ball_radius >= myBar.posY){
+        // if(ball_posX >= myBar.posX && ball_posX <= myBar.posX + myBar.lenght && 
+        // ball_posY + ball_radius == myBar.posY){
+            ball_moveY = -ball_addY ;
+        // }
+        // else if (ball_posY - ball_radius >= SCREEN_HEIGHT) {
+        //     OLED.clearDisplay();
+        //     OLED.setCursor(5,6);
+        //     OLED.setTextSize(2);
+        //     OLED.setTextColor(WHITE);
+        //     OLED.println("Game over!");
+        //     OLED.display();
+        //     delay(5000);
+        //     setup();
+        // }
     }
     OLED.display();
 }
@@ -94,6 +105,4 @@ void loop() {
 void drawBall(){
     OLED.drawCircle(ball_posX, ball_posY, ball_radius, WHITE);
     OLED.fillCircle(ball_posX, ball_posY, ball_radius, WHITE);
-    OLED.display();
 }
-
